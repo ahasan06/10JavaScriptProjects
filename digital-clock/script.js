d9410fa87394da3ce0f3118cd8f07713f17e6f17
Md Ahasan Habib Nahid <141475377+ahasan06@users.noreply.github.com>
2024-03-26 11:52:14 +0600

    digital clock done

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -1,15 +1,17 @@
 function clock() {
-    let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    let MonthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     let today = new Date();
-    document.getElementById('Date').innerHTML = (dayNames[today.getDay()] + " " + today.getDate() + " " + MonthName[today.getMonth()] + " " + today.getFullYear());
+    document.getElementById('Date').innerHTML = (dayNames[today.getDay()] + " " + today.getDate() + " " + monthNames[today.getMonth()] + " " + today.getFullYear());
 
     let h = today.getHours();
     let m = today.getMinutes();
     let s = today.getSeconds();
-
+    let hour  = h -  12;
     let day = h < 11 ? 'AM' : 'PM';
+    
+    h = h > 12 ?  hour : h;
 
     h = h < 10 ? '0' + h : h;
     m = m < 10 ? '0' + m : m;
@@ -20,12 +22,7 @@ function clock() {
     document.getElementById("sec").innerHTML = s;
     document.getElementById("day").innerHTML = day;
 
-
- 
-    setInterval(clock, 400);
-   
+    setInterval(clock, 400);   
 }
 
 clock();
-
-
