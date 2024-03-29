aebacde01bcc9cf5ad8be8bb8d8b77c8c5f85f58
Md Ahasan Habib Nahid <141475377+ahasan06@users.noreply.github.com>
2024-03-30 02:16:44 +0600

    qoute_generator
    html done
    css done
    js done need to fix social btn

--- Quote_generator/script.js
+++ Quote_generator/script.js
@@ -1,8 +1,52 @@
-let qouteText = document.getElementById(".quote");
-let qouteButton = document.getElementById("quote_button");
+let quoteText = document.getElementById("quote");
 
-function randomQuote(){
-    console.log("Clicked");
+let quote_author = document.getElementById("quote_author");
+let quoteButton = document.getElementById("quote_button");
+
+let soundBtn = document.getElementById("soundBtn");
+let copyBtn = document.getElementById("copyBtn");
+let socialBtn = document.getElementById("socialBtn");
+
+function randomQuote() {
+
+    quoteButton.innerText = "Loding..."
+
+    fetch('https://api.quotable.io/random')
+        .then(res => res.json())
+        .then(result => {
+            console.log(result);
+            quoteText.innerText = result.content
+            quote_author.innerText = result.author
+            quoteButton.innerText = "New Qoute"
+        });
 }
 
-qouteButton.addEventListener("click",randomQuote);
+
+soundBtn.addEventListener("click", () => {
+    let textToSpeak = quoteText.innerText + " by " + quote_author.innerText;
+    let utterance = new SpeechSynthesisUtterance(textToSpeak);
+    let voices = window.speechSynthesis.getVoices();
+    console.log(voices);
+    let femaleVoice = voices.find(voice => voice.name === 'Google हिन्दी');
+    if (femaleVoice) {
+        utterance.voice = femaleVoice;
+    }
+    speechSynthesis.speak(utterance);
+})
+
+copyBtn.addEventListener("click", () => {
+    let textContent = `${quoteText.innerText}
+by ${quote_author.innerText}`;
+    navigator.clipboard.writeText(textContent);
+})
+
+socialBtn.addEventListener("click", () => {
+
+    let textToShare = `${quoteText.innerText} by ${quote_author.innerText}`;
+    // Encode the text for URL
+    let encodedText = encodeURIComponent(textToShare);
+    let facebookShareUrl = `https://www.facebook.com/dialog/share?app_id=YourAppId&display=popup&quote=${encodedText}&href=${window.location.href}`;
+    window.open(facebookShareUrl, '_blank');
+});
+
+quoteButton.addEventListener("click", randomQuote);
