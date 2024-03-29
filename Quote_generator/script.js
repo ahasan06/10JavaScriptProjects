081188ed3d58d110aff81480a9254dde7943697b
Md Ahasan Habib Nahid <141475377+ahasan06@users.noreply.github.com>
2024-03-29 22:32:03 +0600

    Random_qoute
    html css done

---
+++ Quote_generator/script.js
@@ -0,0 +1,8 @@
+let qouteText = document.getElementById(".quote");
+let qouteButton = document.getElementById("quote_button");
+
+function randomQuote(){
+    console.log("Clicked");
+}
+
+qouteButton.addEventListener("click",randomQuote);
